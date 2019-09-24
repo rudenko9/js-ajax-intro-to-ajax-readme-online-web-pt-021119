860838ce39baa6f3c82ad5b5954f8ed8aa316c5b
Nataliya R <n.rudenko@gmail.com>
2019-09-24 20:00:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
-<<<<<<< HEAD
 }
 
 function showRepositories() {
